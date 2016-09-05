4d7673d5b224b25c136a1cdf2c5faf928a475ccf
alialrahem <ali_alrahem@yahoo.com>
2016-09-05 13:15:14 -0400

    added my first assignment

---
+++ Solition Application4/ConsoleApplication4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication4
+{
+    class Program
+    {
+        string myName = "Donload";
+        string myInitial = "Peter";
+        string mylastName = "Brown";
+        string myfullName = "Doload Peter Brown";
+        int age = 45;
+        bool isCitizen = true;
+        bool canVote = true;
+        double heightFeet = 0.466;
+        double heightInches = 5.6;
+        double totalHeightCM = 14.22;
+        
+          
+        
+        static void Main(string[] args)
+        {
+        
+        }
+    }
+}
